eb165c82455b3cb0e961196fcdf97f21c52ca4d0
Tommy Asai <shallow.well11@gmail.com>
2023-08-23 16:10:41 +0900

    Remove the seed script since it's necessary only for the first time

--- start.sh
+++ start.sh
@@ -8,19 +8,4 @@
 set -ex
 npx prisma migrate deploy
 npm install tsconfig-paths
-
-# Set ENV to create a user in seed.ts
-export EMAIL=shallow.well11@gmail.com
-export PASSWORD=cMAl4e3W60NmbTE
-# Add a swap file to help with the memory limit during the build.
-# https://community.fly.io/t/prisma-sqlite-causes-an-out-of-memory-error-on-deploy/11039/2
-fallocate -l 256M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile
-echo 10 > /proc/sys/vm/swappiness
-swapon /swapfile
-npm run seed
-swapoff /swapfile
-rm /swapfile
-
 npm run start
