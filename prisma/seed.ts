facb815951b113b77c9426844ada0332306e0e04
Tommy Asai <shallow.well11@gmail.com>
2023-08-24 11:39:00 +0900

    Remove unnecessary seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,51 +5,6 @@ import invariant from "tiny-invariant";
 const prisma = new PrismaClient();
 
 async function seed() {
-  const posts = [
-    {
-      slug: "my-second-post",
-      title: "My Second Post",
-      markdown: `
-  # This is my second post
-  
-  Isn't it great?
-      `.trim(),
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtapes I Made Just For You",
-      markdown: `
-  # 90s Mixtape
-  
-  - I wish (Skee-Lo)
-  - This Is How We Do It (Montell Jordan)
-  - Everlong (Foo Fighters)
-  - Ms. Jackson (Outkast)
-  - Interstate Love Song (Stone Temple Pilots)
-  - Killing Me Softly With His Song (Fugees, Ms. Lauryn Hill)
-  - Just a Friend (Biz Markie)
-  - The Man Who Sold The World (Nirvana)
-  - Semi-Charmed Life (Third Eye Blind)
-  - ...Baby One More Time (Britney Spears)
-  - Better Man (Pearl Jam)
-  - It's All Coming Back to Me Now (Céline Dion)
-  - This Kiss (Faith Hill)
-  - Fly Away (Lenny Kravits)
-  - Scar Tissue (Red Hot Chili Peppers)
-  - Santa Monica (Everclear)
-  - C'mon N' Ride it (Quad City DJ's)
-      `.trim(),
-    },
-  ];
-
-  for (const post of posts) {
-    await prisma.post.upsert({
-      where: { slug: post.slug },
-      update: post,
-      create: post,
-    });
-  }
-
   const password = process.env.PASSWORD;
   const email = process.env.EMAIL;
 
@@ -58,7 +13,7 @@ async function seed() {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  return prisma.user.create({
+  prisma.user.create({
     data: {
       email,
       password: {
