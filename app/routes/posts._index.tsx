0d4931476ce4fd3b930b81b4830a43ac41954814
Tommy Asai <shallow.well11@gmail.com>
2023-09-02 00:35:43 +0900

    Refactor /posts page

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,8 +1,8 @@
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { getPostListings } from "~/models/post.server";
-import { useOptionalAdminUser } from "~/utils";
+import { PostsList } from "~/components/PostList";
 
 type LoaderData = {
   posts: Awaited<ReturnType<typeof getPostListings>>;
@@ -15,29 +15,13 @@ export const loader: LoaderFunction = async () => {
 
 export default function PostsRoute() {
   const { posts } = useLoaderData() as LoaderData;
-  const adminUser = useOptionalAdminUser();
 
   return (
-    <main>
-      <h1>Posts</h1>
-      {adminUser ? (
-        <Link to="admin" className="text-red-600 underline">
-          Admin
-        </Link>
-      ) : null}
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link
-              to={post.slug}
-              prefetch="intent"
-              className="text-blue-600 underline"
-            >
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <main className="w-full">
+      <div className="md:flex md:justify-between md:items-center">
+        <h1>Posts</h1>
+      </div>
+      <PostsList posts={posts} />
     </main>
   );
 }
