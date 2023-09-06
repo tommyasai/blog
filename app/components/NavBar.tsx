691c37424b585c8b43714d4fa07be780d9739631
Tommy Asai <shallow.well11@gmail.com>
2023-09-06 21:13:51 +0900

    The text size of the navbar was too small and not properly centered

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 export const NavBar = () => {
   return (
     <nav>
-      <ul className="list-none flex space-x-4">
+      <ul className="text-2xl list-none flex space-x-4 m-2 p-0">
         <li>
           <Link to="/posts">Posts</Link>
         </li>
