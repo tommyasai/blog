f4b0567c9a92068844dfae4219ea6682071b66d9
Tommy Asai <shallow.well11@gmail.com>
2023-09-07 15:41:06 +0900

    Use remix-seo to add the Robots.txt and sitemap

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,16 +1,20 @@
 import type { EntryContext, HandleDataRequestFunction } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
-// import { otherRootRouteHandlers } from "./utils/otherRootRoutes.server";
+import { otherRootRouteHandlers } from "./utils/otherRouteRoutes.server";
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext
+  remixContext: EntryContext,
 ) {
+  for (const handler of otherRootRouteHandlers) {
+    const otherRouteResponse = await handler(request, remixContext);
+    if (otherRouteResponse) return otherRouteResponse;
+  }
   const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+    <RemixServer context={remixContext} url={request.url} />,
   );
 
   responseHeaders.set("Content-Type", "text/html");
@@ -25,7 +29,7 @@ export default function handleRequest(
 }
 
 export const handleDataRequest: HandleDataRequestFunction = (
-  response: Response
+  response: Response,
 ) => {
   if (process.env.NODE_ENV !== "development") {
     response.headers.set("Cache-Control", "max-age=300, s-maxage=31556952");
